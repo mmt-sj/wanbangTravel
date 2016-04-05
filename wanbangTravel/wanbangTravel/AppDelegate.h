5f40e34b0b0b217721cf5a2cbab9cd3f918e55ae
刘旺旺 <435658081@qq.com>
2016-04-05 17:15:59 +0800

    init

---
+++ wanbangTravel/wanbangTravel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wanbangTravel
+//
+//  Created by hlen on 16/4/5.
+//  Copyright © 2016年 mmt&sf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wanbangTravel/wanbangTravel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wanbangTravel
+//
+//  Created by hlen on 16/4/5.
+//  Copyright © 2016年 mmt&sf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
